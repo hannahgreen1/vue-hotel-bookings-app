bb9831a863f5c971f3acefe4c8a177b1816054fc
hannahgreen1 <hannahgreen94@icloud.com>
2021-01-20 10:14:20 +0000

    app and main sorted

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hotel Bookings App</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { eventBus } from '@/main';
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+      bookings: []
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
